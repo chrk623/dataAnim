0e9b7de515305ba0477bea68e44c4215b719ba38
Charco <phui285@gmail.com>
2019-04-16 21:54:05 +1200

    man for join_anim

--- R/join_anim.R
+++ R/join_anim.R
@@ -1,12 +1,25 @@
-#' <Add Title>
+#' Joining Animation
 #'
-#' <Add Description>
-#'
+#' @param join_type
+#' @param x Table x, to pass to join.
+#' @param y Table y, to pass to join.
+#' @param by a character value of the variable to join by.
+#' @param speed Speed of the animation.
+#' @param width Width of the animation frame.
+#' @param height Height of the animation frame.
+#' @param show_msg A logical value indicating whether to show instructional messages in the animation.
+#' @param design \code{svydb.design} object.
+#' @description
+#' Compute quantiles from survey data sets.
+#' @examples
+#' data(datoy1)
+#' join_anim(join_type = "left", speed = 1, x = datoy1$x, y = datoy1$y, by = "Name", show_msg = T)
+#' join_anim(join_type = "inner", speed = 1, x = datoy1$x, y = datoy1$y, by = "Name", show_msg = F)
+#' @author Charco Hui
 #' @import htmlwidgets
 #'
 #' @export
 join_anim <- function(join_type = "left", x, y, by, speed = 1, width = NULL, height = NULL, show_msg = F) {
-
   # forward options using x
   data = list(data = process_join(x = x, y = y, key = by, complete_action = FALSE,
                                   show_msg = show_msg, join_type = join_type, asJSON = TRUE),
